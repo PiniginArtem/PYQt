f5e59959e740eb40338a4d4cd6bade573c57585b
PiniginArtem <Piniginartem206@gmail.com>
2023-12-27 16:10:57 +0300

    Initial commit

---
+++ Практика 4/a_Повторение + Разбор/a_paint_rect.py
@@ -0,0 +1,45 @@
+from PySide6 import QtCore, QtWidgets, QtGui
+
+
+class Widget(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.x1, self.y1 = 0, 0
+        self.x2, self.y2 = 0, 0
+
+    def paintEvent(self, event: QtGui.QPaintEvent) -> None:
+        painter = QtGui.QPainter(self)
+
+        pen = QtGui.QPen()
+        pen.setColor(QtCore.Qt.GlobalColor.red)
+        pen.setWidth(5)
+        pen.setBrush(QtCore.Qt.BrushStyle.Dense4Pattern)
+
+        brush = QtGui.QBrush()
+        brush.setColor(QtCore.Qt.GlobalColor.red)
+        brush.setStyle(QtCore.Qt.BrushStyle.SolidPattern)
+
+        painter.setPen(pen)
+        painter.setBrush(brush)
+
+        w, h = self.x2 - self.x1, self.y2 - self.y1
+        painter.drawRect(self.x1, self.y1, w, h)
+
+    def mouseMoveEvent(self, event: QtGui.QMouseEvent) -> None:
+        self.x2, self.y2 = event.position().x(), event.position().y()
+        self.repaint()
+
+    def mousePressEvent(self, event: QtGui.QMouseEvent) -> None:
+        self.x1, self.y1 = event.position().x(), event.position().y()
+        self.x2, self.y2 = event.position().x(), event.position().y()
+        self.repaint()
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication()
+
+    myapp = Widget()
+    myapp.show()
+
+    app.exec()
